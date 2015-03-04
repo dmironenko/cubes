7ed37c8e4bba5112703b6d2b895fb000706a97de
dmironenko <dmironenko@cybervisiontech.com>
2015-03-04 14:15:18 +0200

    Tests to matrix turn right method

--- src/main/java/com/cubes/Side.java
+++ src/main/java/com/cubes/Side.java
@@ -1,21 +1,18 @@
 package com.cubes;
 
+import java.util.Arrays;
 import java.util.List;
 
-/**
- * @author Dmytro Myronenko dmironenko@cybervisiontech.com
- *         Date: 3/4/15.
- */
 public class Side {
     static final int SIDE_SIZE = 5;
 
-    boolean side[][] = new boolean[SIDE_SIZE][SIDE_SIZE];
+    private final boolean sides[][] = new boolean[SIDE_SIZE][SIDE_SIZE];
 
     public Side(List<String> lines) {
         for (int i = 0; i < SIDE_SIZE; i++) {
             String line = lines.get(i);
             for (int j = 0; j < SIDE_SIZE; j++) {
-                side[i][j] = line.charAt(j) == 'o';
+                sides[i][j] = line.charAt(j) == 'o';
             }
         }
     }
@@ -27,15 +24,19 @@ public class Side {
         for (int i = 0; i < SIDE_SIZE / 2; i++) {
             int last = SIDE_SIZE - i - 1;
             for (int j = i; j < last; j++) {
-                boolean tmp = side[i][j];
-                side[i][j] = side[last - j][i];
+                boolean tmp = sides[i][j];
+                sides[i][j] = sides[last - j][i];
 
-                side[last][last - j] = side[j][last];
-                side[last - j][i] = side[last][last - j];
+                sides[last - j][i] = sides[last][last - j];
+                sides[last][last - j] = sides[j][last];
 
-                side[j][last] = tmp;
+                sides[j][last] = tmp;
             }
         }
+    }
+
+    public boolean[][] getSides() {
+        return Arrays.copyOf(sides, sides.length);
     }
 
     @Override
@@ -44,7 +45,7 @@ public class Side {
 
         for (int i = 0; i < SIDE_SIZE; i++) {
             for (int j = 0; j < SIDE_SIZE; j++) {
-                sb.append(side[i][j] ? 'o' : ' ');
+                sb.append(sides[i][j] ? 'o' : ' ');
             }
             sb.append(System.lineSeparator());
         }
---
+++ src/main/java/com/util/FileUtils.java
@@ -0,0 +1,45 @@
+package com.util;
+
+import com.cubes.Side;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class FileUtils {
+
+    private FileUtils() {
+        throw new AssertionError("Instantiating utility class");
+    }
+
+    public static List<Side> readSides(InputStream stream) throws IOException {
+        List<Side> result = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+            for (int i = 0; i < 2; i++) {
+                List<String> s1 = new ArrayList<>();
+                List<String> s2 = new ArrayList<>();
+                List<String> s3 = new ArrayList<>();
+
+                for (int j = 0; j < 5; j++) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        throw new IllegalStateException("Input file has invalid format");
+                    }
+
+                    s1.add(line.substring(0, 5));
+                    s2.add(line.substring(5, 10));
+                    s3.add(line.substring(10, 15));
+
+
+                }
+                result.addAll(Arrays.asList(new Side(s1), new Side(s2), new Side(s3)));
+            }
+        }
+        return result;
+    }
+}
